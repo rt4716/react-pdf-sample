3118044f22098e14b7b8982b7d2f0b26e09f3576
rt4716 <ryuichi.takashima@gmail.com>
2024-07-09 18:58:46 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Document, Page, pdfjs } from 'react-pdf';
+import samplePDF1 from './sample1.pdf';
+import samplePDF2 from './sample2.pdf';
 import './App.css';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
 
-function App() {
+// pdfjs-distからpdf.worker.min.jsファイルへのパスを設定
+pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
+
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="pdf-container">
+        <div className="pdf">
+          <Document file={samplePDF1}>
+            {Array.from(new Array(5), (el, index) => (
+              <Page key={`samplePDF1-page-${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+        <div className="pdf">
+          <Document file={samplePDF2}>
+            {Array.from(new Array(5), (el, index) => (
+              <Page key={`samplePDF2-page-${index + 1}`} pageNumber={index + 1} />
+            ))}
+          </Document>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
